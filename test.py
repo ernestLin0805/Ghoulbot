50390c49d535670cafa4448971770eb558f92575
celloboiErnest <ernestl8502@gmal.com>
2018-08-15 10:21:53 -0700

    Test Fixed

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ class test:
             character = discord.Embed(title = "Class: {}".format(stats[id]['Class']), description = "", colour = discord.Colour.blue())
             character.add_field(name = "Attack: ", value = "{}".format(stats[id]['Attack']))
             character.add_field(name = "Health: ", value = "{}".format(stats[id]['Health']))
-            character.add_field(name = "Defense: ", value = "{}".format(stats[id]['Defense']))
+            character.add_field(name = "Speed: ", value = "{}".format(stats[id]['Speed']))
             await self.bot.send_file(ctx.message.channel, stats[id]["Picture"])
             await self.bot.send_message(ctx.message.channel, embed=character)
 def setup(bot):
